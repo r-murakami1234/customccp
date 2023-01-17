10a67afc3cd62138678bf78a63184e7de78005e7
r-murakami1234 <r-murakami@logical.co.jp>
2023-01-17 15:42:48 +0900

    jschange

--- custom-ccp.js
+++ custom-ccp.js
@@ -66,7 +66,7 @@ function init() {
 					
 					fetch(apiURL, requestOptions)
 					.then(response => response.text())
-    				.then(result => alert(result[[2]])
+    				.then(result => alert(result[[7]])
 					)
 					console.log('コンタクト属性を取得: 名前 = \"' + result[[0]] + '\"\n');
 				// }
