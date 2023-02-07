ca036b6b1ed1b2781a29c8c24b66d31650690298
r-murakami1234 <r-murakami@logical.co.jp>
2023-02-07 23:19:12 +0900

    resulttest

--- outbound.js
+++ outbound.js
@@ -21,11 +21,30 @@ function init() {
     });
     // 発信ボタンを押すと、input フォームに入力された電話番号へ発信
     connect.agent(function (agent) {
-        document.getElementById('dial2').onclick = function() {
+        document.getElementById('dial').onclick = function() {
             var phoneNumber = document.getElementById('phone_number').value;
-            var phoneNumber2 = document.getElementById('dial2').value;
             phoneNumber = '+81' + phoneNumber.slice(1);
             console.log(phoneNumber);
+            
+            // endpoint インスタンス作成　
+            var endpoint = connect.Endpoint.byPhoneNumber(phoneNumber);
+
+            var queueArn = "arn:aws:connect:ap-northeast-1:831461333044:instance/63eb6a17-a9da-4caf-a4bf-8ad2f5cf887e/queue/fff1d521-eedb-4fdd-9e41-d355e36a6789";
+
+            agent.connect(endpoint, {
+                queueARN: queueArn,
+                success: function() { console.log("outbound call connected"); },
+                failure: function(err) {
+                    console.log("outbound call connection failed");
+                    console.log(err);
+                }
+            });
+        }
+    });
+    // クリック発信
+    connect.agent(function (agent) {
+        document.getElementById('clickDial').onclick = function() {
+            var phoneNumber2 = document.getElementById('clickDial');
             console.log(phoneNumber2);
 
             // endpoint インスタンス作成　
@@ -42,7 +61,7 @@ function init() {
                 }
             });
         }
-    });
+    })
 
     // 切断ボタンが押されたら、電話を切る
     connect.contact(function (contact) {
