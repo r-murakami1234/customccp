c47ad58b138177a987f8aec15743b21e437e8b10
r-murakami1234 <r-murakami@logical.co.jp>
2023-02-13 14:04:58 +0900

    reroadchange

---
+++ import-csv.js
@@ -0,0 +1,9 @@
+function init() {
+    var importURL = "https://gntsf0si0f.execute-api.ap-northeast-1.amazonaws.com/POST"
+
+    function addanswer() {
+        fetch(importURL, requestOptions)
+        answer = document.getElementById("answer");
+        answer.innertext = "CSVファイルを更新しました。"
+    }
+}
